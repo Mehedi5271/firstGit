7550c1d7e811ca4de917522b5a337d47f4f619dd
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2023-07-10 01:04:14 +0600

    fst

---
+++ practice.php
@@ -0,0 +1,17 @@
+<?php
+
+// variable data type (scalar)
+
+// integer,string,float,boolean
+
+
+$name = "pondit";
+$name = "PHP";
+
+$isactive = true;
+
+echo $name;
+
+// if else , for, foreach
+
+?>
