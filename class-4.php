6a83d703fbc808a48407508e09934c7ff6dc8851
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2023-07-10 22:25:46 +0600

    class 4

---
+++ class-4.php
@@ -0,0 +1,34 @@
+<?php
+$name = [
+    'mehedi',
+    'bappy',
+    'himel'
+];
+
+echo "<pre";
+
+print_r($name);
+
+echo "<hr>";
+
+$student = [
+   'm' =>'mehedi',
+    'b' =>'bappy',
+    'h' =>'himel'
+];
+
+echo "<pre";
+
+print_r($student);
+
+echo "<hr>";
+
+
+$department = [
+    'cse' => array('Seam', 'Erfan'),
+    'bba' => ['Ananna', 'Mehedi']
+];
+
+
+
+?>
